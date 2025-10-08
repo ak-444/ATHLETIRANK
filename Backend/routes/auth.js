33b7ca5fb60cb5863642968555dc7caf45f5596e
niel doroja <neilemon42@gmail.com>
2025-10-09 03:20:28 +0800

    update

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -18,7 +18,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function sendEmail({ to, subject, html }) {
     try {
         const data = await resend.emails.send({
-            from: 'AthletiRank <noreply@athletirank.com>',
+           from: 'AthletiRank <onboarding@resend.dev>', 
             to,
             subject,
             html,
